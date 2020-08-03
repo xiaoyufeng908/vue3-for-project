683e2e5fe2a86ccc1cf7878b0a88c595b300c2b4
xiaoyufeng <360311908@qq.com>
2020-08-03 20:43:38 +0800

    commit 20200803 vue3项目搭建

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <HelloWorld :msg="msg" :count="count"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
+interface propsReactive {
+  msg: String,
+  count: Number
+}
+export default defineComponent({
+  setup() {
+    const data:propsReactive = reactive<propsReactive>({
+      msg: 'HELLO WORLD!',
+      count: 0
+    })
+    return {
+      data
+    }
+  },
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
